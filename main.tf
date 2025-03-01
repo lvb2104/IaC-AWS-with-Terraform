0413b51aa343fa1ea4284a8c79a619291263e114
Bao Le <levanbao2105@gmail.com>
2025-03-01 06:24:49 +0000

    Edit main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,9 @@
+provider "aws" {
+  region = var.region
+}
+
 module "ec2_instance" {
   source          = "./modules/ec2"
-  region          = var.region_module
   ami             = var.ami_module
   instance_type   = var.instance_type_module
   key_name        = var.key_name_module
